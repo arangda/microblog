eede2af6155e2e035b05439d7b94cdb553db3e73
arangda <273890638@qq.com>
2020-03-16 16:21:22 +0800

    1621

--- app/tasks.py
+++ app/tasks.py
@@ -1,22 +1,15 @@
+import json
+import sys
 import time
+from flask import render_template
 from rq import get_current_job
-from app import db,create_app
-from app.models import Task
+from app import create_app, db
+from app.models import User, Post, Task
+from app.email import send_email
 
 app = create_app()
 app.app_context().push()
 
-def example(seconds):
-    job = get_current_job()
-    print('Starting task')
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i /seconds
-        job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print('Task completed')
 
 def _set_task_progress(progress):
     job = get_current_job()
@@ -29,6 +22,7 @@ def _set_task_progress(progress):
         if progress >= 100:
             task.complete = True
         db.session.commit()
+
 
 def export_posts(user_id):
     try:
@@ -44,13 +38,15 @@ def export_posts(user_id):
             i += 1
             _set_task_progress(100 * i // total_posts)
 
-            send_email('[Microblog] Your blog posts',
-            sender=app.config['ADMINS'][0], recipients=[user.email],
-            text_body=render_template('email/export_posts.txt', user=user),
-            html_body=render_template('email/export_posts.html', user=user),
-            attachments=[('posts.json', 'application/json',
-                            json.dumps({'posts': data}, indent=4))],
-            sync=True)
+        send_email('[Microblog] Your blog posts',
+                sender=app.config['ADMINS'][0], recipients=[user.email],
+                text_body=render_template('email/export_posts.txt', user=user),
+                html_body=render_template('email/export_posts.html',
+                                          user=user),
+                attachments=[('posts.json', 'application/json',
+                              json.dumps({'posts': data}, indent=4))],
+                sync=True)
     except:
         _set_task_progress(100)
         app.logger.error('Unhandled exception', exc_info=sys.exc_info())
+        print(sys.exc_info())
