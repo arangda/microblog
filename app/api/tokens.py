9141fde81f32c7c3e155b7d91f29da883272bff3
arangda <273890638@qq.com>
2020-03-18 16:55:10 +0800

    1655

--- app/api/tokens.py
+++ app/api/tokens.py
@@ -2,6 +2,7 @@ from flask import jsonify, g
 from app import db
 from app.api import bp
 from app.api.auth import basic_auth
+from app.api.auth import token_auth
 
 @bp.route('/tokens', methods=['POST'])
 @basic_auth.login_required
@@ -12,8 +13,10 @@ def get_token():
 
 
 
-def get_token():
-    pass
 
+@bp.route('/tokens',methods=['DELETE'])
+@token_auth.login_required
 def revoke_token():
-    pass
+    g.current_user.revoke_token()
+    db.session.commit()
+    return '',204
